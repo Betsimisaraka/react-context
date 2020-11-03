3c2b6febefde1bf6949ecb07671ca96957f418dc
Anita RASOA <anita.ras@onja.org>
2020-11-03 11:16:58 +0300

    The first react-context

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ThemeContext from './ThemeContext';
+
+ReactDOM.render(
+    <ThemeContext.Provider value={"dark"}>
+        <App />
+    </ThemeContext.Provider>, 
+    document.getElementById('root')
+);
