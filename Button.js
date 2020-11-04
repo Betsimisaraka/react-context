814bc5284ce8210514de89fe8815c9dd7d0bd452
Anita RASOA <anita.ras@onja.org>
2020-11-04 09:29:17 +0300

    Using an object into the value in the provider

--- Button.js
+++ Button.js
@@ -3,9 +3,9 @@ import { ThemeContextConsumer } from './ThemeContext';
 
 function Button() {
 	return (
-		<ThemeContextConsumer>
-			{theme => (
-				<button className={`${theme}-theme`}>Switch Theme</button>
+		<ThemeContextConsumer> 
+			{context => (
+				<button onClick={context.toggleTheme} className={`${context.theme}-theme`}>Switch Theme</button>
 			)}
 		</ThemeContextConsumer>
 	)
